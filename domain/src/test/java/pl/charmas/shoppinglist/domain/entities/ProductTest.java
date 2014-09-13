b6a76e45554d6fd161a924b926912945a00a700c
Michał Charmas <michal@charmas.pl>
2014-09-13 16:20:47 +0200

    Added sample test to domain.

---
+++ domain/src/test/java/pl/charmas/shoppinglist/domain/entities/ProductTest.java
@@ -0,0 +1,13 @@
+package pl.charmas.shoppinglist.domain.entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+    @Test
+    public void testShouldBeBoughtWhenMarkedAsBought() throws Exception {
+        Product boughtProduct = new Product(0, "sample name", false).markBought();
+        assertEquals(true, boughtProduct.isBought());
+    }
+}
