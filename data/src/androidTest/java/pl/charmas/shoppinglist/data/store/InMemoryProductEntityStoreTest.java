010f609db9c64098b2618f080573d6ff8c4e130c
Michał Charmas <michal@charmas.pl>
2014-09-13 16:23:21 +0200

    Added sample data test.

---
+++ data/src/androidTest/java/pl/charmas/shoppinglist/data/store/InMemoryProductEntityStoreTest.java
@@ -0,0 +1,12 @@
+package pl.charmas.shoppinglist.data.store;
+
+import junit.framework.TestCase;
+
+public class InMemoryProductEntityStoreTest extends TestCase {
+    public void testShouldContain20DemoProductsAfterCreation() throws Exception {
+        InMemoryProductEntityStore store = new InMemoryProductEntityStore();
+        int items = store.listAll().size();
+
+        assertEquals(20, items);
+    }
+}
